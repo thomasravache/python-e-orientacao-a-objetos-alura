ca79c6a37b7de2f67fb4033d72deb80659b96c7d
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2023-03-17 11:58:35 -0300

    feat: Comparando variáveis

--- projects/jogos/adivinhacao.py
+++ projects/jogos/adivinhacao.py
@@ -1,1 +1,18 @@
+print('******************')
 print('Bem vindo no jogo de adivinhação!')
+print('******************')
+
+numero_secreto = 42
+
+# tipo string
+chute_tipo_string = input('Digite o seu número: ')
+chute = int(chute_tipo_string)
+
+print('Você digitou ', chute)
+
+# Necessária a identação de 4 espaços, senão o python não entende. Ele se baseia na identação do código
+
+if (chute == numero_secreto):
+    print('Você acertou!')
+else:
+    print('Você errou!')
